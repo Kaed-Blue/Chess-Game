34c9cebd7dd06e4543fa7d534508aa4729c11dc6
Kaed_Blue <paradaisclash.1383@gmail.com>
2025-09-20 20:27:04 +0330

    added assets

--- chess_game.py
+++ chess_game.py
@@ -12,23 +12,23 @@ cols = windows_height // cell_size
 screen = pygame.display.set_mode((windows_width, windows_height))
 
 
-def init_board():
-    # fmt: off
-    pieces_dict = {
-        "rook": [(0, 0), (7, 7), (7, 0), (0, 7)],
-        "knight": [(0, 1), (0, 6), (7, 1), (7, 6)],
-        "queen": [(0, 3), (7, 3)],
-        "king": [(0, 4), (7, 4)],
-        "bishop": [(0, 2), (0, 5), (7, 2), (7, 5)],
-        "pawn": [(1, 0), (1, 1), (1, 2),(1, 3),
-                 (1, 4), (1, 5), (1, 6), (1, 7),
-                 (6, 0),(6, 1), (6, 2), (6, 3),
-                 (6, 4), (6, 5), (6, 6), (6, 7),
-        ],
+pieces_dict = {
+    "rook": [(0, 0), (7, 7), (7, 0), (0, 7)],
+    "knight": [(0, 1), (0, 6), (7, 1), (7, 6)],
+    "queen": [(0, 3), (7, 3)],
+    "king": [(0, 4), (7, 4)],
+    "bishop": [(0, 2), (0, 5), (7, 2), (7, 5)],
+    "pawn": [(1, 0), (1, 1), (1, 2), (1, 3),
+             (1, 4), (1, 5), (1, 6), (1, 7),
+             (6, 0), (6, 1), (6, 2), (6, 3),
+             (6, 4), (6, 5), (6, 6), (6, 7),
+             ],
     }
     # fmt: on
 
-    board = []
+board = []
+def init_board():
+    # fmt: off
     for row in range(int(rows)):  # col = x, row = y
         board.append([])
         for col in range(int(cols)):
@@ -65,8 +65,12 @@ init_board()
 
 
 def draw_board():
-    pass
+    screen.fill((0, 0, 0))
 
+    for row in range(int(rows)):
+        for col in range(int(cols)):
+            if board[row][col]["occupied"] == True:
+                
 
 def get_acceptable_moves():
     pass
