0c20466d27a975ab3567d994fea2627c2f16cd9b
Kaed_Blue <paradaisclash.1383@gmail.com>
2025-09-24 20:19:41 +0330

    works but coded really poorly
    going for refactoring

--- Engine.py
+++ Engine.py
@@ -1,6 +1,3 @@
-# from GUI import draw_board
-
-
 class Engine:
     def __init__(self):
         # fmt: off
@@ -14,6 +11,7 @@ class Engine:
                      "R","N","B","Q","K","B","N","R"]
         # fmt: on
         self.delta_pos = {"first_click": "", "second_click": ""}
+        self.valid_moves = []
 
     def get_piece(self, index):
         return self.board[index]
@@ -23,12 +21,14 @@ class Engine:
         selected_row = click_pos[1] // 100
         index = (selected_row * 8) + selected_col
         # NOT DYNAMIC don't change the board size #TODO: get the click position dynamiclly
+
         if self.delta_pos["first_click"] == "":
             if self.board[index] != ".":
                 self.delta_pos["first_click"] = index
-                valid_moves = self.get_valid_moves(index)
+                self.get_valid_moves(index)
         else:
             self.delta_pos["second_click"] = index
+            print(self.delta_pos)
 
             if self.board[self.delta_pos["second_click"]].isalpha():
                 if (
@@ -36,19 +36,26 @@ class Engine:
                     == self.board[self.delta_pos["second_click"]].isupper()
                 ):
 
+                    print("happend")
                     self.delta_pos["first_click"] = self.delta_pos["second_click"]
-                    self.delta_pos["second_click"] = ()
+                    self.delta_pos["second_click"] = ""
+                    self.get_valid_moves(int(self.delta_pos["first_click"]))
                     return
 
-            self.board[self.delta_pos["second_click"]] = self.board[self.delta_pos["first_click"]] # fmt: skip
-            self.board[self.delta_pos["first_click"]] = "."
+            if self.delta_pos["second_click"] in self.valid_moves:
+                self.board[self.delta_pos["second_click"]] = self.board[self.delta_pos["first_click"]] # fmt: skip
+                self.board[self.delta_pos["first_click"]] = "."
 
-            # print(delta_pos)
+            # print(self.delta_pos)
             self.delta_pos["first_click"] = ""
             self.delta_pos["second_click"] = ""
+            self.valid_moves = []
 
     def get_valid_moves(self, index):
-        print("valid moves")
+        piece_movements = {}
+        if self.board[index] == "P":
+            if self.board[index - 8] == ".":
+                self.valid_moves.append(index - 8)
 
 
 # draw_board(board=board)
